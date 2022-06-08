9900fdd991e2b6ba8d81aae8ba5c83fbc5ec363e
WozGeek <89479520+WozGeek@users.noreply.github.com>
2022-06-08 00:26:54 +0000

    Delete Constante.java

--- Constante.java
+++
@@ -1,7 +0,0 @@
-package com.defitech.tp_vente.modele;
-
-public class Constante {
-    public static final String ETAT_NORMAL="seuil normal";
-    public static final String ETAT_CRITIQUE="seuil critique";
-    public static final String ETAT_DANGER="Danger";
-}

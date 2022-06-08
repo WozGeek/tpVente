3350e39bdda9b447bd866736c02776d3b6ca26b9
WozGeek <89479520+WozGeek@users.noreply.github.com>
2022-06-08 00:24:45 +0000

    Add files via upload

---
+++ src1/main/java/com/defitech/tp_vente/modele/Constante.java
@@ -0,0 +1,7 @@
+package com.defitech.tp_vente.modele;
+
+public class Constante {
+    public static final String ETAT_NORMAL="seuil normal";
+    public static final String ETAT_CRITIQUE="seuil critique";
+    public static final String ETAT_DANGER="Danger";
+}

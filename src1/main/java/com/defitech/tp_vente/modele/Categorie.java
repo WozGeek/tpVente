f2f0ba9d9a1c6c8ae8eeb19ae13b4298067e7745
WozGeek <89479520+WozGeek@users.noreply.github.com>
2022-06-08 00:23:50 +0000

    Add files via upload

---
+++ src1/main/java/com/defitech/tp_vente/modele/Categorie.java
@@ -0,0 +1,22 @@
+package com.defitech.tp_vente.modele;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private  String designation;
+
+}
